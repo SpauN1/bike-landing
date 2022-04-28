ca849e6437a963c68767097f49d22c8a8380246c
Andrei Vasiutovich <andreispaun7@gmail.com>
2022-04-28 11:01:31 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function ibg() {
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector("img")) {
+			ibg[i].style.backgroundImage =
+				"url(" + ibg[i].querySelector("img").getAttribute("src") + ")";
+		}
+	}
+}
+
+ibg();
+
+const swiper = new Swiper(".swiper", {
+	// Optional parameters
+	direction: "horizontal",
+	loop: true,
+	autoHeight: true,
+	// If we need pagination
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+});
+
+
